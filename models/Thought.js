8ed9de194d486ed3b04a5140f8d595a4c3174f70
lorreina <slorreina369@gmail.com>
2022-09-04 11:19:52 -0700

    reaction schema created

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,34 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model, Types} = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId:{
+            type:Schema.Types.ObjectId,
+            default:() => new Types.ObjectId()
+        },
+        reactionBody:{
+            type:String,
+            required:true,
+            max:280
+        },
+        username:{
+            type:String,
+            required:true
+        },
+        createdAt:{
+            type:Date,
+            default:Date.now, 
+            get:createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON:{
+            getters:true
+        }
+    }
+);
 
 const ThoughtSchema = new Schema (
-    //  username, reactions
     {
         thoughtText:{
             type:String,
@@ -17,17 +44,17 @@ const ThoughtSchema = new Schema (
         username:{
             type:String,
             required:true
-        }
-        /*
-         reaction:[ReactionSchema]
-         */
+        },
+        reaction:[ReactionSchema]
+    },
+    {
+        toJSON:{
+            virtuals:true,
+            getters:true
+        },
+        id:false
     }
-    /**
-     * toJSON:{
-     *      probably:?
-     * }
-     */
-)
+);
 
 const Thought = model('Thought', ThoughtSchema);
 
